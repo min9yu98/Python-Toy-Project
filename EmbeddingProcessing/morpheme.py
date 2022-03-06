be8fba334642b932bca894cb59d2cb377d1d63e4
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-03-07 08:10:41 +0900

    Update morpheme.py

--- EmbeddingProcessing/morpheme.py
+++ EmbeddingProcessing/morpheme.py
@@ -7,13 +7,13 @@ special = re.compile(r'[^ A-Za-z0-9가-힣+]')
 concat = []
 corpus = []
 
-text_file = 'C:/Users/kmkkm/Desktop/ChosunJoongang_total.txt'
+text_file = '불러올 경로'
 f = open(text_file, 'r', encoding='utf-8')
 lines = f.readlines()
 for line in lines:
   corpus.append(line)
 f.close()
-print(corpus[-1])
+# print(corpus[-1])
 
 clean_corpus = []
 for text in corpus:
@@ -26,8 +26,8 @@ for text in processing:
 pos_text = []
 pos_text.append(token_corpus)
 
-print(pos_text[0][-1])
-with open('drive/MyDrive/Embedding/chosunJoongang_total_pos.txt', 'a', encoding='utf-8') as f:
+# print(pos_text[0][-1])
+with open('저장할 ', 'a', encoding='utf-8') as f:
   for j in range(len(pos_text[0])):
     t = ' '.join(pos_text[0][j])
     f.write(t + '\n')
