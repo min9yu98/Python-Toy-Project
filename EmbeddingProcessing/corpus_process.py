3f5cabd5f270d0c2ee2a858dc24f2da8f3d7b54b
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-03-07 08:09:25 +0900

    Update corpus_process.py

--- EmbeddingProcessing/corpus_process.py
+++ EmbeddingProcessing/corpus_process.py
@@ -1,10 +1,10 @@
 from konlpy.tag import Okt
 import re
 
-with open('C:/Users/kmkkm/Desktop/SBS_total.txt', 'a', encoding='utf-8') as file:  # 입력
+with open('저장경로', 'a', encoding='utf-8') as file:  # 입력
     for i in range(2):
         for j in range(1, 13):
-            path1 = 'C:/Users/kmkkm/Desktop/sbs/202' + str(i) + '_' + str(j) + '_sbs.txt'
+            path1 = '불러올 경로'
             f = open(path1, 'r', encoding='utf-8')
             lines = f.readlines()
             for line in lines:
