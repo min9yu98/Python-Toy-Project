883f02f18336ceab1b7c14e065ac738359e21841
min9yu98 <76439068+min9yu98@users.noreply.github.com>
2022-03-11 18:18:40 +0900

    Update instagram.py

--- crawling/instagram.py
+++ crawling/instagram.py
@@ -1,4 +1,6 @@
 #입력 필요 : 크롬 드라이버 설치 경로, 조선일보 ID, PassWord, 키워드, 기사를 저장할 공간의 경로
+# pip install bs4, pyautogui, selenium, lxml, requests
+
 import re
 
 import pyautogui
@@ -33,11 +35,11 @@ time.sleep(4)
 driver.find_element(By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/span/a[1]/button').click()
 
 
-def login():
+def login(): # 로그인
     driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input').click()
-    driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys('인스타id')
+    driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys('insta ID') #ID
     driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').click()
-    driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys('인스타 비번')
+    driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys('insta PWD') #PWD
     driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button').click()
     time.sleep(5)
     pyautogui.click(472, 642)
@@ -47,10 +49,10 @@ def login():
 
 
 
-def insta_urls():
+def insta_urls(): # 페이지에 url 긁어오기
     html = driver.page_source
     bs = BeautifulSoup(html, 'lxml')
-    source = str(bs.find_all('article', {'class', 'KC1QD'})).split('"')
+    source = str(bs.find_all('div')).split('"')
     insta_urls_ = []
     for e in source:
         if '/p/' in e:
@@ -59,7 +61,7 @@ def insta_urls():
 
 
 
-def insta_contents(url):
+def insta_contents(url): # 긁어온 url에서 내용 빼오기
     driver.get(url)
     contents_html = driver.page_source
     contents_bs = BeautifulSoup(contents_html, 'lxml')
@@ -68,7 +70,7 @@ def insta_contents(url):
 
 
 
-def waiting():
+def waiting(): # 스크롤해서 데이터 더 불러오기
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
     time.sleep(7)
 
@@ -77,14 +79,24 @@ def waiting():
 time.sleep(3)
 login()
 
-for i in range(3):
+rep = []
+
+while True:
     waiting()
+    urls = insta_urls()
+    for e in urls:
+        if e not in rep:
+            rep.append(e)
+    if len(rep) == 300:
+        break
 
-urls = insta_urls()
-
-for url in urls:
+print(rep)
+idx = 0
+for url in rep:
     try:
-        print(insta_contents(url))
+        print(str(idx) + "----------" + insta_contents(url))
+        time.sleep(random.randrange(1, 2))
+        idx += 1
     except AttributeError:
         continue
 
